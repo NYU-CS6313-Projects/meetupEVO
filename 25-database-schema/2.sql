a741bf87c1614f521504c2d3e2c8e11645dd0a41
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2015-03-27 18:15:42 -0400

    right order of creating tables, references, ids fixed

--- 25-database-schema/2.sql
+++ 25-database-schema/2.sql
@@ -1,76 +1,12 @@
+/* drop tables in reverse order of creating */
 
-CREATE TABLE groups (
-  id_group BIGINT primary key NOT NULL,
-  -- has one organizer
-  id_organizer BIGINT NULL references members(id_member),
-  name_organizer VARCHAR(100)  NULL,
-  -- is in one category
-  id_category BIGINT  NULL,
-  name_category VARCHAR(100)  NULL,
-  shortname_category VARCHAR(100)  NULL,
-  -- core group attributes
-  name VARCHAR(100)  NULL,
-  description VARCHAR(100)  NULL,
-  link VARCHAR(100)  NULL,
-  who VARCHAR(100)  NULL,
-  join_mode VARCHAR(100)  NULL,
-  created VARCHAR(1000)  NULL,
-  created_wday BIGINT  NULL,
-  urlname VARCHAR(100)  NULL,
-  visibility VARCHAR(100)  NULL,
-  -- attributes that change over time
-  no_members BIGINT NULL,
-  rating FLOAT NULL,
-  -- location
-  city VARCHAR(100)  NULL,
-  lat FLOAT  NULL,
-  lon FLOAT  NULL,
-  state VARCHAR(100)  NULL,
-  country VARCHAR(100)  NULL,
-  timezone VARCHAR(100)  NULL
-);
+DROP TABLE rsvps;
+DROP TABLE events;
+DROP TABLE groups;
+DROP TABLE members;
+DROP TABLE venues;
 
-CREATE TABLE events (
-  id_event BIGINT primary key NULL,
-  -- belongs to one group
-  id_group BIGINT NOT NULL references groups(id_group),
-  -- has one venue
-  id_venue BIGINT NOT NULL references venue(id_venue),
-  -- core event attributes
-  name VARCHAR(100)  NULL,
-  description VARCHAR(100)  NULL,
-  time VARCHAR(1000)  NULL,
-  time_wday BIGINT  NULL,
-  utc_offset BIGINT  NULL,
-  event_url VARCHAR(100)  NULL,
-  duration FLOAT  NULL,
-  status VARCHAR(100)  NULL,
-  description VARCHAR(100)  NULL,
-  why VARCHAR(100)  NULL,
-  how_to_find_us VARCHAR(100)  NULL,
-  rating FLOAT  NULL,
-  visibility VARCHAR(100)  NULL,
-  created VARCHAR(1000)  NULL,
-  created_wday BIGINT  NULL,
-  updated BIGINT  NULL,
-  photo_url VARCHAR(100)  NULL,
-  -- fee
-  label_fee VARCHAR(100)  NULL,
-  amount_fee FLOAT  NULL,
-  accepts_fee VARCHAR(100)  NULL,
-  currency_fee VARCHAR(100)  NULL,
-  description_fee VARCHAR(100)  NULL,
-  required_fee VARCHAR(100)  NULL,
-  -- counts
-  yes_rsvp_count BIGINT  NULL,
-  waitlist_count BIGINT  NULL,
-  maybe_rsvp_count BIGINT  NULL,
-  count_rating FLOAT  NULL,
-  average_rating FLOAT  NULL,
-  headcount BIGINT  NULL,
-);
-
-CREATE TABLE venue (
+CREATE TABLE venues (
   id_venue BIGINT primary key NULL,
   name VARCHAR(100)  NULL,
   address_1 VARCHAR(100)  NULL,
@@ -78,53 +14,123 @@ CREATE TABLE venue (
   phone VARCHAR(100)  NULL,
   lon FLOAT  NULL,
   lat FLOAT  NULL,
-  zip VARCHAR(100)  NULL,
-  city VARCHAR(100)  NULL
-  state VARCHAR(100)  NULL,
-  country VARCHAR(100)  NULL,
-  repinned VARCHAR(100)  NULL
+  zip VARCHAR(10)  NULL,
+  city VARCHAR(50)  NULL,
+  state VARCHAR(50)  NULL,
+  country VARCHAR(50)  NULL,
+  repinned BOOLEAN  NULL
 );
-
 CREATE TABLE members (
-  id_member BIGINT primary key NULL,
-  -- belongs to many groups!!! should be a separate table!!
-  id_group VARCHAR(100)  NULL,
-  -- core member attributes
+  id_member VARCHAR(20) primary key NULL,
+  /* belongs to many groups!!! should be a separate table!! */
+  id_group VARCHAR(20)  NULL,
+  /* core member attributes */
   name VARCHAR(100)  NULL,
-  status VARCHAR(100)  NULL,
-  bio VARCHAR(100)  NULL,
+  bio VARCHAR(500)  NULL,
   visited BIGINT  NULL,
   joined BIGINT  NULL,
   link VARCHAR(100)  NULL,
-  -- location
+  /* location */
   city VARCHAR(100)  NULL,
   lon FLOAT  NULL,
   lat FLOAT  NULL,
   state VARCHAR(100)  NULL,
   country VARCHAR(100)  NULL,
   hometown VARCHAR(100)  NULL,
-  -- photo
+  /* photo */
   id_photo FLOAT  NULL,
   link_photo VARCHAR(100)  NULL,
   thumb_link_photo VARCHAR(100)  NULL,
-  highres_link_photo VARCHAR(100)  NULL,
+  highres_link_photo VARCHAR(100)  NULL
 );
+CREATE TABLE groups (
+  id_group VARCHAR(30) primary key NOT NULL,
+  /* has one organizer */
+  id_organizer VARCHAR(30) references members(id_member) NOT NULL,  
+  name_organizer VARCHAR(100)  NULL,
+  /* is in one category */
+  id_category BIGINT  NULL,
+  name_category VARCHAR(100)  NULL,
+  shortname_category VARCHAR(100)  NULL,
+  /* core group attributes */
+  name VARCHAR(100)  NULL,
+  description VARCHAR(100)  NULL,
+  link VARCHAR(100)  NULL,
+  who VARCHAR(100)  NULL,
+  join_mode VARCHAR(100)  NULL,
+  created TIMESTAMP NULL,
+  created_wday SMALLINT  NULL,
+  urlname VARCHAR(250)  NULL,
+  visibility VARCHAR(20)  NULL,
+  /* attributes that change over time */
+  no_members INT NULL,
+  rating FLOAT NULL,
+  /* location */
+  city VARCHAR(100)  NULL,
+  lat FLOAT  NULL,
+  lon FLOAT  NULL,
+  state VARCHAR(100)  NULL,
+  country VARCHAR(100)  NULL,
+  timezone VARCHAR(100)  NULL
+);
+CREATE TABLE events (
+  id_event VARCHAR(30) primary key NULL,
+  /* belongs to one group */
+  id_group VARCHAR(30) REFERENCES groups(id_group) NOT NULL,
+  /* has one venue */
+  id_venue BIGINT REFERENCES venues(id_venue) NOT NULL,
+  /* core event attributes */
+  name VARCHAR(100)  NULL,
+  description VARCHAR(500)  NULL,
+  time TIMESTAMP NULL,
+  time_wday SMALLINT  NULL,
+  utc_offset SMALLINT  NULL,
+  event_url VARCHAR(150)  NULL,
+  duration FLOAT  NULL,
+  status VARCHAR(100)  NULL,
+  why VARCHAR(260)  NULL,
+  how_to_find_us VARCHAR(250)  NULL,
+  rating FLOAT  NULL,
+  venue_visibility VARCHAR(30)  NULL,
+  created TIMESTAMP NULL,
+  created_wday SMALLINT  NULL,
+  updated TIMESTAMP  NULL,
+  photo_url VARCHAR(250)  NULL,
+  /* fee */
+  label_fee VARCHAR(100)  NULL,
+  amount_fee FLOAT  NULL,
+  accepts_fee VARCHAR(20)  NULL,
+  currency_fee VARCHAR(100)  NULL,
+  description_fee VARCHAR(100)  NULL,
+  required_fee VARCHAR(100)  NULL,
+  /* counts */
+  yes_rsvp_count INT  NULL,
+  waitlist_count INT  NULL,
+  maybe_rsvp_count INT  NULL,
+  count_rating INT  NULL,
+  average_rating FLOAT  NULL,
+  headcount BIGINT  NULL
+);
+COMMENT ON COLUMN events.duration IS 'Event duration in milliseconds';
+COMMENT ON COLUMN events.why IS 'We should do this because...';
+COMMENT ON COLUMN events.venue_visibility IS 'Controls the visibility of venue. May be one of "public" or "members"';
+
 
 CREATE TABLE rsvps (
   rsvp_id BIGINT primary key NULL,
-  -- this is a join table between member and event
-  id_member BIGINT NOT NULL references members(id_member),
-  id_event VARCHAR(100) NOT NULL references events(id_event),
-  -- core attributes
-  response VARCHAR(100)  NULL,
-  watching FLOAT  NULL,
-  comments VARCHAR(100)  NULL,
+  /* this is a join table between member and event */
+  id_member VARCHAR(30) REFERENCES members (id_member),
+  id_event VARCHAR(30) REFERENCES events (id_event),
+  /* core attributes */
+  response VARCHAR(20)  NULL,
+  watching BOOLEAN  NULL,
+  comments VARCHAR(250)  NULL,
   guests BIGINT  NULL,
-  -- time
-  created VARCHAR(1000)  NULL,
-  created_time VARCHAR(100)  NULL,
-  created_wday BIGINT  NULL,
-  mtime VARCHAR(1000)  NULL,
-  mtime_time VARCHAR(100)  NULL,
-  mtime_wday BIGINT  NULL
+  /* time */
+  created TIMESTAMP NULL,
+  created_time TIME  NULL,
+  created_wday SMALLINT  NULL,
+  mtime TIMESTAMP NULL,
+  mtime_time TIME NULL,
+  mtime_wday SMALLINT  NULL
 );
