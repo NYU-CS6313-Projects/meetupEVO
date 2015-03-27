51fed28dc79e2037fc61126224f3a508852595d2
Brigitte Jellinek <bjelli@gmail.com>
2015-03-27 07:29:56 -0400

    extract events and groups as csv for tableau analysis

--- 20-exploring/utilities.py
+++ 20-exploring/utilities.py
@@ -1,8 +1,20 @@
 import numpy as np
+from numpy import dtype
 import pandas as pd
 import os
+from glob import glob
+
+
+def date_col(df, col):
+    if not col in df.columns:
+      return df
+    df[col] = pd.to_datetime(df[col],unit='ms')  # just guessing here
+    df[col + "_wday"] = df[col].apply(lambda x: x.weekday())
+    return df
 
 def flatten_col(df, col):
+    if not col in df.columns:
+      return df
     try: 
         sub_df = df[col].apply(pd.Series)
         sub_cols = [ c for c in list(sub_df.columns.values) if type(c) != int]
@@ -31,43 +43,140 @@ def flatten_col(df, col):
         return df
 
 
+class CategoryReader:
+  def __init__(self, dir="../10-data/groups_new/", ext=".json", do_max = False, do_text = True, max_columns = {
+      'city': dtype('O'),
+      'country': dtype('O'),
+      'created': dtype('<M8[ns]'),
+      'created_wday': dtype('int64'),
+      'description': dtype('O'),
+      'filename': dtype('O'),
+      'id_category': dtype('int64'),
+      'id_main': dtype('int64'),
+      'join_mode': dtype('O'),
+      'lat': dtype('float64'),
+      'link': dtype('O'),
+      'lon': dtype('float64'),
+      'member_id_organizer': dtype('int64'),
+      'members': dtype('int64'),
+      'name_category': dtype('O'),
+      'name_main': dtype('O'),
+      'name_organizer': dtype('O'),
+      'rating': dtype('float64'),
+      'shortname_category': dtype('O'),
+      'state': dtype('O'),
+      'timezone': dtype('O'),
+      'urlname': dtype('O'),
+      'visibility': dtype('O'),
+      'who': dtype('O')
+    }):
+    self.filenames = glob( dir + "*" + ext )
+    self.cursor = 0
+    self.dir = dir
+    self.ext = ext
+    self.do_max = do_max
+    self.do_text = do_text
+    self.max_columns = max_columns
+
+  def __iter__(self):
+    return self
+
+  def next(self):
+    # find the next (non-empty) file on my list, or stop
+    while self.cursor < len( self.filenames ) and os.path.getsize(self.filenames[self.cursor])==0:
+      self.cursor += 1
+    if self.cursor >= len( self.filenames ):
+      raise StopIteration
+
+    # found a file
+    filename = self.filenames[ self.cursor ] 
+    self.cursor += 1
+ 
+    df = pd.read_json(filename)
+    if len(df) == 0:
+        return df
+    df[u'filename'] = filename.replace(self.ext,"").replace(self.dir,"")
+    df = flatten_col(df, "category") 
+    df = flatten_col(df, 'organizer')
+    df = date_col(df, "created") 
+    del(df["group_photo"])
+    del(df["topics"])
+
+    if not self.do_text:
+      del(df["description"])
+
+    if not self.do_max:
+      return df
+    full_df = pd.DataFrame({k: pd.Series(dtype=v) for k,v in self.max_columns.items()})
+    for k in self.max_columns.keys():
+	if k in df.columns:
+	   full_df[k] = df[k]
+    if not self.do_text:
+      del(full_df["description"])
+    return full_df
+    
+
 class EventReader:
   def __init__(self,group_ids, dir="../10-data/events_updated/", ext=".json", do_max = False, max_columns = {
-	u'rating': np.dtype('float64'),
-        u'event_url': np.dtype('O'),
-        u'updated': np.dtype('int64'),
-        u'urlname_group': np.dtype('O'),
-        u'duration': np.dtype('float64'),
-        u'id_group': np.dtype('int64'),
-        u'id': np.dtype('int64'),
-        u'fee': np.dtype('O'),
-        u'group': np.dtype('O'),
-        u'average_rating': np.dtype('float64'),
-        u'rsvp_limit': np.dtype('float64'),
-        u'lon_group': np.dtype('float64'),
-        u'created_main': np.dtype('int64'),
-        u'filename': np.dtype('O'),
-        u'yes_rsvp_count': np.dtype('int64'),
-        u'id_main': np.dtype('int64'),
-        u'lat_group': np.dtype('float64'),
-        u'waitlist_count': np.dtype('int64'),
-        u'count_rating': np.dtype('float64'),
-        u'maybe_rsvp_count': np.dtype('int64'),
-        u'join_mode_group': np.dtype('O'),
-        u'status': np.dtype('O'),
-        u'utc_offset': np.dtype('int64'),
-        u'visibility': np.dtype('O'),
-        u'who_group': np.dtype('O'),
-        u'created_group': np.dtype('int64'),
-        u'why': np.dtype('O'),
-        u'photo_url': np.dtype('O'),
-        u'name_main': np.dtype('O'),
-        u'created': np.dtype('int64'),
-        u'name_group': np.dtype('O'),
-        u'venue': np.dtype('O'),
-        u'time': np.dtype('int64'),
-        u'headcount': np.dtype('int64'),
-        u'how_to_find_us': np.dtype('O')
+    0: dtype('float64'),
+    u'rating': dtype('float64'),
+    u'event_url': dtype('O'),
+    u'who_group': dtype('O'),
+    u'lon_venue': dtype('float64'),
+    u'photo_url': dtype('O'),
+    u'amount_fee': dtype('float64'),
+    u'id_group': dtype('int64'),
+    u'accepts_fee': dtype('O'),
+    'time_wday': dtype('int64'),
+    u'group': dtype('O'),
+    u'currency_fee': dtype('O'),
+    u'lon_group': dtype('float64'),
+    u'zip_venue': dtype('O'),
+    u'yes_rsvp_count': dtype('int64'),
+    u'city_venue': dtype('O'),
+    u'lat_group': dtype('float64'),
+    u'count_rating': dtype('float64'),
+    u'join_mode_group': dtype('O'),
+    u'description_fee': dtype('O'),
+    u'updated': dtype('int64'),
+    '0_main': dtype('float64'),
+    u'required_fee': dtype('O'),
+    u'visibility': dtype('O'),
+    u'name_venue': dtype('O'),
+    u'created_group': dtype('<M8[ns]'),
+    'created_group_wday': dtype('int64'),
+    'created_wday': dtype('int64'),
+    u'name_main': dtype('O'),
+    u'name_group': dtype('O'),
+    u'how_to_find_us': dtype('O'),
+    u'utc_offset': dtype('int64'),
+    u'urlname_group': dtype('O'),
+    u'address_1_venue': dtype('O'),
+    u'duration': dtype('float64'),
+    u'description_main': dtype('O'),
+    u'id': dtype('int64'),
+    u'rsvp_limit': dtype('float64'),
+    u'lat_venue': dtype('float64'),
+    u'created_main': dtype('int64'),
+    u'filename': dtype('O'),
+    u'average_rating': dtype('float64'),
+    u'id_main': dtype('int64'),
+    u'country_venue': dtype('O'),
+    u'waitlist_count': dtype('int64'),
+    u'maybe_rsvp_count': dtype('int64'),
+    u'status': dtype('O'),
+    '0_venue': dtype('float64'),
+    u'state_venue': dtype('O'),
+    u'description': dtype('O'),
+    u'repinned_venue': dtype('O'),
+    u'phone_venue': dtype('O'),
+    u'why': dtype('O'),
+    u'id_venue': dtype('float64'),
+    u'created': dtype('<M8[ns]'),
+    u'label_fee': dtype('O'),
+    u'time': dtype('<M8[ns]'),
+    u'address_2_venue': dtype('O'),
+    u'headcount': dtype('int64')
   }):
     self.filenames = [ dir + f.strip() + ext for f in group_ids]
     self.cursor = 0
@@ -96,13 +205,21 @@ class EventReader:
     df[u'filename'] = filename.replace(self.ext,"").replace(self.dir,"")
     df = flatten_col(df, "group")
     df = flatten_col(df, "rating") 
+    df = flatten_col(df, "fee") 
+    df = flatten_col(df, "venue") 
+    df = date_col(df, "created") 
+    df = date_col(df, "created_group") 
+    df = date_col(df, "time") 
 
     # copy over to new df?
     #return df[[ self.max_columns ]]
     if not self.do_max:
       return df
-    print "ERROR IMPLEMENT ME"
-    exit()
+    full_df = pd.DataFrame({k: pd.Series(dtype=v) for k,v in self.max_columns.items()})
+    for k in self.max_columns.keys():
+	if k in df.columns:
+	   full_df[k] = df[k]
+    return full_df
     
 
 class GroupMemberReader:
