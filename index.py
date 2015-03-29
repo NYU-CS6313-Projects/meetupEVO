c35a3806ce3a737956c579303cc878016b67b1f5
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2015-03-29 08:40:23 -0400

    tile map, word cloud with jquery, about page

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-import os, psycopg2, psycopg2.extras, json
+import os, psycopg2, psycopg2.extras, json, re, random
 from flask import Flask, Response, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -14,13 +15,24 @@ def before_request():
     app.logger.error('Error: %s' % inst)
     abort(500)
 # ====================================================================================
+@app.route('/about.html')
+def about():
+  return render_template("about.html", title = "About this Site")
+
+# ====================================================================================
+@app.route('/map.html')
+def map():
+  return render_template("map.html", title = "Map of Locations", description = "Showing no data as of yet.")
+
+# ====================================================================================
 @app.route('/groups/count_cities.html')
-@app.route('/groups/count_states.html')
-def horizontal_barchart_tsv_html():
-  return render_template("horizontal-barchart-tsv.html", data_url = request.path.replace('.html', '.tsv') )
+def groups_count_cities_html():
+  return render_template("horizontal-barchart-tsv.html", 
+      data_url = request.path.replace('.html', '.tsv'), 
+      title = "Count Groups in Cities" )
 
 @app.route('/groups/count_cities.tsv')
-def count_cities():
+def groups_count_cities_tsv():
     try: 
       result = "label\tfrequency"
       g.db_cursor.execute("""select city as label,count(*) as count from groups group by city order by count desc""")
@@ -30,10 +42,16 @@ def count_cities():
       app.logger.error('exception %s' % ex)
       pass
     return Response(result, mimetype='text/plain')
-    #return Response(result, mimetype='text/tab-separated-values')
+
+# ====================================================================================
+@app.route('/groups/count_states.html')
+def groups_count_states_html():
+  return render_template("horizontal-barchart-tsv.html", 
+      data_url = request.path.replace('.html', '.tsv'), 
+      title = "Count Groups in State")
 
 @app.route('/groups/count_states.tsv')
-def count_states():
+def groups_count_states_tsv():
     try: 
       result = "label\tfrequency"
       g.db_cursor.execute("""select state as label,count(*) as count from groups group by state order by count desc""")
@@ -42,11 +60,14 @@ def count_states():
     except Exception as ex:
       app.logger.error('exception %s' % ex)
       pass
-    #return Response(result, mimetype='text/plain')
-    return Response(result, mimetype='text/tab-separated-values')
+    return Response(result, mimetype='text/plain')
+    
+@app.route('/map.html')
+def map():
+  return render_template("map.html", title = "Map of Locations", description = "Showing no data as of yet.")
 
 @app.route('/groups/group_size_histogram.json')
-def group_size_histogram():
+def group_size_histogram_json():
     try: 
       g.db_cursor.execute("""
         select width_bucket(no_members,0,50000,30) as bucket, count(*) as count from groups group by 1 order by 1
@@ -75,7 +96,25 @@ def index():
       (select count(*) as no_venues from venues) AS d,
       (select count(*) as no_rsvps from rsvps) AS e
       """)
-    return render_template("index.html", db = g.db_cursor.fetchone())
+    counts = g.db_cursor.fetchone()
+    g.db_cursor.execute("""select name from groups where random() < 1""")
+    word_counter = Counter()
+    total_count = 0
+    for name in g.db_cursor.fetchall():
+      for w in name['name'].split(" "):
+        w = w.lower()
+        if re.match("^\w+$", w) and not w in ['of', 'the', 'and', 'in', 'meetup', 'for', 'club', 'group', 'city', 'nyc', 'new', 'york']:
+            # 'nyc', 'new', 'york', 'brooklyn', 'jersey', 'ny', 'westchester', 'nj','manhattan', 'staten', 'island'
+          total_count += 1
+          word_counter[ w ] += 1 
+    word_weight={}
+    for w in word_counter.keys():
+      p = 100.0 * word_counter[ w ]  / total_count
+      if p > 0.2:
+        word_weight[ w ] = p 
+    word_counter_keys = word_weight.keys()
+    random.shuffle(word_counter_keys)
+    return render_template("index.html", counts = counts, word_counter = word_weight, word_counter_keys = word_counter_keys )
 # ====================================================================================
 if __name__ == '__main__':
   app.run(debug=True)
